893863dc523b761ddc47125605b94553c98e0dcd
gswm1205 <dreamer.hwi@gmail.com>
2022-07-05 00:16:17 +0900

    AfterEach

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -3,14 +3,21 @@ package hello.hellospring.repository;
 import hello.hellospring.domain.Member;
 //import org.junit.jupiter.api.Assertions;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class MemoryMemberRepositoryTest {
-    MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
 
     @Test
     public void save(){
@@ -24,5 +31,41 @@ public class MemoryMemberRepositoryTest {
         //System.out.println("result = " + (result == member));
         //Assertions.assertEquals(member, result); // expect, actual
         assertThat(member).isEqualTo(result); //요즘 거, option+Enter 하면 Assertions 사라짐, static import
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member(); //shift + F6 복사 변수 이름 같이 바뀜
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result  = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring1");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        // 2번 돌리면 findByName 이 이상함
+        /*
+         findAll 이든 findByName 이든 실행 순서는 보장되지 않음
+         모든 테스트는 순서와 상관없이 모든 메서드를 돌림
+        */
     }
 }

fcdacef699f5d96686620e0f6bc385d37eb72eea
gswm1205 <dreamer.hwi@gmail.com>
2022-07-04 23:48:52 +0900

    TestCase 검증

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,12 +1,28 @@
 package hello.hellospring.repository;
 
+import hello.hellospring.domain.Member;
+//import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class MemoryMemberRepositoryTest {
     MemberRepository repository = new MemoryMemberRepository();
 
     @Test
     public void save(){
-        
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        //검증 : 저장한 member 와 DB에 저장된 것과 완전히 똑같으면 참
+        Member result = repository.findById(member.getId()).get();
+        //System.out.println("result = " + (result == member));
+        //Assertions.assertEquals(member, result); // expect, actual
+        assertThat(member).isEqualTo(result); //요즘 거, option+Enter 하면 Assertions 사라짐, static import
     }
 }

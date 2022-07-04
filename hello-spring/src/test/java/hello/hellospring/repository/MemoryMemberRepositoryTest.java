a11fc40ef827fbfb556fefea9e969cc5cd475133
gswm1205 <dreamer.hwi@gmail.com>
2022-07-04 23:20:54 +0900

    make TestCase first

---
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,12 @@
+package hello.hellospring.repository;
+
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        
+    }
+}

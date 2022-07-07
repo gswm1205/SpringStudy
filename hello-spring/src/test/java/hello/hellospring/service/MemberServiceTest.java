eb94e882fc90d4326c01ad3503fd0d9c23d39738
gswm1205 <dreamer.hwi@gmail.com>
2022-07-07 23:05:21 +0900

    make memberServiceTest

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,29 @@
+package hello.hellospring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//test만드는 단축키 : command + shift + t
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    // test는 과감하게 한글로 바꿔도 됨 shift + f6
+    @Test
+    void 회원가입() {
+        //givien
+        Member member = new
+        //when
+
+        //then
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}

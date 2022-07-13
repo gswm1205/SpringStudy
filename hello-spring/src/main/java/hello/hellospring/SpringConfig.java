ef963e56e8a831078d719fbe8f8b27b1667e84e2
gswm1205 <dreamer.hwi@gmail.com>
2022-07-13 09:32:42 +0900

    SpringConfig

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,9 +1,25 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //직접 spring bean 등록 : 직접 설정 파일에 등록
+//controller 는 설정할 수 있는 것이 아니라서 놔두는 것
 @Configuration
 public class SpringConfig {
+
+    //spring bean 등록
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 
 }
